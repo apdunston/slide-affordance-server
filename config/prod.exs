5ae199da58bf1c682a04bb5d22504bdd778921bb
Adrian Dunston <adunston@netsertive.com>
2019-08-19 13:20:43 -0400

    Got some config from tutorial. Trying that now.

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,15 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :slide_affordance_server, SlideAffordanceServerWeb.Endpoint,
-  url: [host: "slide-affordance-server.gigalixirapp.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+
+config :gigalixir_getting_started, GigalixirGettingStarted.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
 
 # Do not print debug messages in production
 config :logger, level: :info
