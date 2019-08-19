941536721e059a51f41d3771f5b2ae50ba133e37
Adrian Dunston <adunston@netsertive.com>
2019-08-19 13:13:26 -0400

    Updating prod endpoint host for sockets again, but this time with the subdomain

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :slide_affordance_server, SlideAffordanceServerWeb.Endpoint,
-  url: [host: "gigalixirapp.com", port: 80],
+  url: [host: "slide-affordance-server.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
