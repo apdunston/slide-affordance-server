2c5465b62a89871c85ea9de908d02cf103cf22fb
Adrian Dunston <adunston@netsertive.com>
2019-08-19 12:43:37 -0400

    Removing ETS as I'm using an Agent instead

--- mix.exs
+++ mix.exs
@@ -43,8 +43,7 @@ defmodule SlideAffordanceServer.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
-      {:ets, "~> 0.7.3"}
+      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"}
     ]
   end
 
